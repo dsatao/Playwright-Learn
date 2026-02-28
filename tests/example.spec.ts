e9075848bd784884ce86bf83efb50badfed1e408
Dipak Satao <101934282+dsatao@users.noreply.github.com>
2026-02-28 16:14:52 -0600

    work on adding correct test cases.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,14 +4,26 @@ test.describe('Homepage Tests', () => {
 
   test('login loads successfully', async ({ page }) => {
     await page.goto('/');
-    await expect(page).toHaveURL("http://52.5.210.245/Account/Login");
+    await expect(page).toHaveURL("http://localhost:5227/Account/Login");
     console.log('✅ Login page loaded');
+    await page.locator('#username').fill('admin');
+    await page.locator('#password').fill('admin');
+    await page.locator('button[type="submit"]').click();
+    await expect(page).toHaveURL("http://localhost:5227/");
+    console.log('✅ Login successful, homepage loaded');
   });
 
- /* test('navigation links are visible', async ({ page }) => {
+  test('logout buttons are visible', async ({ page }) => {
     await page.goto('/');
-    await expect(page.locator('nav')).toBeVisible();
-  });*/
+    await expect(page).toHaveURL("http://localhost:5227/Account/Login");
+    console.log('✅ Login page loaded');
+    await page.locator('#username').fill('admin');
+    await page.locator('#password').fill('admin');
+    await page.locator('button[type="submit"]').click();
+    await expect(page).toHaveURL("http://localhost:5227/");
+    console.log('✅ Login successful, homepage loaded');
+    await expect(page.locator('body > div > form > button')).toBeVisible();
+  });
 
   test('about page loads', async ({ page }) => {
     await page.goto('/Home/About');
